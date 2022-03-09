27bdf2e3894e7b577c42647879cd1b0d4d46b960
Theo zibell <theo@zibell.se>
2022-03-09 10:27:34 +0100

    vet nu vilket obijket jag har

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ function getItem(){
     let itemNumbers = localStorage.getItem('item');
     if(itemNumbers){
         document.querySelector('.shopping span').textContent = itemNumbers;
-    }
+    } 
 }
 
 function cartStorge(){
@@ -22,6 +22,38 @@ function cartStorge(){
     localStorage.setItem('item', itemNumbers + 1);
 
     document.querySelector('.shopping span').textContent = itemNumbers + 1;
+}
+
+function Purchase(item) {
+    switch(item) {
+        case 'Stellaris':
+
+            break;
+        case'Hollow Knight':
+            
+            break;
+        case'DARK SOULS 3':
+
+            break;
+        case'Lost Ark':
+
+            break;
+        case'Brawlhalla':
+
+            break;
+        case'Rust':
+
+            break;
+        case'Scrap Mechanic':
+
+            break;
+        case'Tom Clancys Rainbow Six Siege':
+
+            break;
+
+        default: console.log("item cant be switched: ", item)
+            break;
+    }
 }
 
 getItem();
