078e504df5a1475a052c1d17d31d5a9539ec6830
Theo zibell <theo@zibell.se>
2022-03-11 12:52:53 +0100

    .

--- app.js
+++ app.js
@@ -26,60 +26,6 @@ function Purchase(item) {
     itemNumbers = parseInt(itemNumbers);
     localStorage.setItem(item, itemNumbers + 1);
 }
-    /*
-    switch(item) {
-        case 'Stellaris':
-            var itemNumbers = localStorage.getItem(item) || 0;
-            itemNumbers = parseInt(itemNumbers);
-            localStorage.setItem(item, itemNumbers + 1);
-            break;
-        case 'Manor lords':
-            itemNumbers = localStorage.getItem(item) || 0;
-            itemNumbers = parseInt(itemNumbers);
-            localStorage.setItem(item, itemNumbers + 1);
-            break;
-        case'Hollow Knight':
-            itemNumbers = localStorage.getItem(item) || 0;
-            itemNumbers = parseInt(itemNumbers);
-            localStorage.setItem(item, itemNumbers + 1);
-            break;
-        case'DARK SOULS 3':
-            itemNumbers = localStorage.getItem(item) || 0;
-            itemNumbers = parseInt(itemNumbers);
-            localStorage.setItem(item, itemNumbers + 1);
-            break;
-        case'Lost Ark':
-            itemNumbers = localStorage.getItem(item) || 0;
-            itemNumbers = parseInt(itemNumbers);
-            localStorage.setItem(item, itemNumbers + 1);
-            break;
-        case'Brawlhalla':
-            itemNumbers = localStorage.getItem(item) || 0;
-            itemNumbers = parseInt(itemNumbers);
-            localStorage.setItem(item, itemNumbers + 1);
-            break;
-        case'Rust':
-            itemNumbers = localStorage.getItem(item) || 0;
-            itemNumbers = parseInt(itemNumbers);
-            localStorage.setItem(item, itemNumbers + 1);
-            break;
-        case'Scrap Mechanic':
-            itemNumbers = localStorage.getItem(item) || 0;
-            itemNumbers = parseInt(itemNumbers);
-            localStorage.setItem(item, itemNumbers + 1);
-            break;
-        case'Tom Clancys Rainbow Six Siege':
-            itemNumbers = localStorage.getItem(item) || 0;
-            itemNumbers = parseInt(itemNumbers);
-            localStorage.setItem(item, itemNumbers + 1);
-            break;
-
-        default: console.log("item cant be switched: ", item)
-            break;
-       
-    }
-     */
-
 
 function displayCart (){
 let cartItem = localStorage.getItem("Stellaris")
